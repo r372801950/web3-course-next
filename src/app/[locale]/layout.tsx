6498eb28cf820e8bf512e5a186c9f3f6dbebaf70
jiang <372801950@qq.com>
2025-05-02 21:58:06 +0800

    next-intl setup with i18n routing

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,27 @@
+
+import {NextIntlClientProvider, hasLocale} from 'next-intl';
+import {notFound} from 'next/navigation';
+import {routing} from '@/i18n/routing';
+import {ReactNode} from "react";
+
+export default async function LocaleLayout({
+  children,
+  params
+}: {
+  children: ReactNode;
+  params: Promise<{ locale: string }>;
+}) {
+  // Ensure that the incoming `locale` is valid
+  const {locale} = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  return (
+    <html lang={locale}>
+    <body>
+      <NextIntlClientProvider>{children}</NextIntlClientProvider>
+    </body>
+    </html>
+  );
+}
